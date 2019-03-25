16f2eb73e2829ca4556e60a216ebf1130a1c59cc
armel kepjio kamwa <armelkepjio@yahoo.fr>
2019-03-25 10:53:19 +0100

    My first php page

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
             </div><br/>
 
             <div id="body">
-            <form action="head-foot.php" method="post">
+            <form action="index.php" method="post">
                 Name: <input type="text" name="name">
                 <input type="submit"><br/>
            
